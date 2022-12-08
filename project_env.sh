c60549a0de278ad2ce7d5065bd8de975ab894f3a
vlfuentes417 <vfuentes@mvcc.edu>
2022-12-08 12:29:00 -0800

    updated files

--- project_env.sh
+++ project_env.sh
@@ -1,5 +1,5 @@
 export PDK_ROOT=$(pwd)/pdks
 export PDK=sky130A
-export OPENLANE_ROOT=$(pwd)/openlane
+export OPENLANE_ROOT=$(pwd)/openlane_code
 export CARAVEL_ROOT=$(pwd)/caravel
 export MCW_ROOT=$CARAVEL_ROOT/mgmt_core_wrapper
